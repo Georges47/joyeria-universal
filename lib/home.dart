cb999a7b871264e8a5978b024c92056b969241df
Georges Stefan <stefangeorges4799@gmail.com>
2023-01-17 20:52:16 -0300

    Improve home section responsiveness

--- lib/home.dart
+++ lib/home.dart
@@ -8,22 +8,25 @@ class Home extends StatelessWidget {
   const Home({super.key, required this.carouselController});
 
   double _cardWidth(double screenWidth) {
-    if (screenWidth > 800) {
-      return (1100 * screenWidth) / (screenWidth + 400);
-    } else {
-      return screenWidth / 1.2;
+    if (screenWidth > 1550) {
+      return screenWidth / 1.75;
+    } else if (screenWidth > 950) {
+      return 886;
+    } else if (screenWidth > 350) {
+      return screenWidth / 1.07;
+    }
+    else {
+      return 327;
     }
   }
 
   double _cardHeight(double screenHeight) {
     if (screenHeight > 1000) {
       return screenHeight / 2;
-    } else if (screenHeight > 800) {
-      return screenHeight / 1.75;
-    } else if (screenHeight > 650) {
-      return screenHeight / 1.5;
+    } else if (screenHeight > 550) {
+      return 0.1333 * screenHeight + 366.6666;
     } else {
-      return screenHeight / 1.25;
+      return 440;
     }
   }
 
@@ -94,50 +97,59 @@ class Home extends StatelessWidget {
                 child: Card(
                   elevation: 8.0,
                   margin: EdgeInsets.zero,
-                  color: Colors.white,
-                  child: Padding(
-                    padding: EdgeInsets.all(screenWidth(context) > 1200 ? 64.0 : 16.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 200,
-                          width: 350,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('assets/images/logo_name_2.png'),
-                              fit: BoxFit.cover,
+                  child: Center(
+                    child: SingleChildScrollView(
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: LimitedBox(
+                          maxWidth: _cardWidth(screenWidth(context)),
+                          child: Padding(
+                            padding: EdgeInsets.all(screenWidth(context) > 1200 ? 64.0 : 16.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 200,
+                                  width: 350,
+                                  decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage('assets/images/logo_name_2.png'),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 18.0,),
+                                Center(
+                                  child: screenWidth(context) > 600 ?
+                                    _text('Visítenos con confianza - Atención esmerada - Precios sin competencia') :
+                                    Column(
+                                      children: [
+                                        _text('Visítenos con confianza'),
+                                        _text('Atención esmerada'),
+                                        _text('Precios sin competencia'),
+                                      ],
+                                    )
+                                ),
+                                // 'El más completo surtido de joyas en acero y finas fantasías'
+                                // 'Representantes de las mas famosas marcas de relojes' (Versace, Fendi, Tissot, Mulco, Nivada)
+                                // 'Desde 1930!',
+                                const SizedBox(height: 32.0,),
+                                Wrap(
+                                  alignment: WrapAlignment.center,
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  spacing: 32.0,
+                                  runSpacing: 16.0,
+                                  children: [
+                                    _sectionButton(0, 'Contacto'),
+                                    _sectionButton(2, 'Ubicación'),
+                                  ],
+                                )
+                              ],
                             ),
                           ),
                         ),
-                        const SizedBox(height: 18.0,),
-                        Center(
-                          child: screenWidth(context) > 600 ?
-                            _text('Visítenos con confianza - Atención esmerada - Precios sin competencia') :
-                            Column(
-                              children: [
-                                _text('Visítenos con confianza'),
-                                _text('Atención esmerada'),
-                                _text('Precios sin competencia'),
-                              ],
-                            )
-                        ),
-                        // 'El más completo surtido de joyas en acero y finas fantasías'
-                        // 'Representantes de las mas famosas marcas de relojes' (Versace, Fendi, Tissot, Mulco, Nivada)
-                        // 'Desde 1930!',
-                        const SizedBox(height: 32.0,),
-                        Wrap(
-                          alignment: WrapAlignment.center,
-                          crossAxisAlignment: WrapCrossAlignment.center,
-                          spacing: 32.0,
-                          runSpacing: 16.0,
-                          children: [
-                            _sectionButton(0, 'Contacto'),
-                            _sectionButton(2, 'Ubicación'),
-                          ],
-                        )
-                      ],
+                      ),
                     ),
                   ),
                 ),
